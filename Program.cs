1f61ee67234f12213e1d194ecbd51cc24b888a9f
Jm Santiago <116279303+sntgjm@users.noreply.github.com>
2022-10-23 14:18:51 +0800

    Activity 5

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = new int[3];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            foreach (int i in numbers)
+            {
+                Console.WriteLine("The number is(shortcut for foreach) = " + i);
+            }
+        }
+    }
+}
